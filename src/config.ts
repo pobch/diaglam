d7fc7f67cfcd1f6537722fed2daed8468f8ecc36
Pob <590650@gmail.com>
2024-11-11 23:36:57 +0700

    Extract font settings into a config file

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,17 @@
 export const CONFIG = {
+  // * roughjs settings
   SEED: 5,
+
+  // * Font settings
+  // * --- Case 1: 'Nanum Pen Script'
+  FONT_SIZE: '1.5rem',
+  FONT_FAMILY: 'Nanum Pen Script',
+  LINE_HEIGHT_FACTOR_IN_CANVAS: 1.2,
+  TEXTAREA_LINE_HEIGHT: 0.8,
+  TEXTAREA_MARGIN_BLOCK_START: '0',
+  // * --- Case 2: 'K2D'
+  // FONT_SIZE: '1rem',
+  // LINE_HEIGHT_FACTOR_IN_CANVAS: 1.6,
+  // TEXTAREA_LINE_HEIGHT: 1.3,
+  // TEXTAREA_MARGIN_BLOCK_START: '-0.25em',
 }
