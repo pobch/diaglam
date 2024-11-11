eb8fed579c9e640688aaee41263cb92174276350
Pob <590650@gmail.com>
2024-11-12 00:20:01 +0700

    Switch font from Nanum to K2D

--- src/config.ts
+++ src/config.ts
@@ -6,16 +6,17 @@ export const CONFIG = {
 
   // * Font settings
   // * --- Case 1: 'Nanum Pen Script'
-  FONT_SIZE: '1.5rem',
-  FONT_FAMILY: 'Nanum Pen Script',
-  LINE_HEIGHT_FACTOR_IN_CANVAS: 1.2,
-  TEXTAREA_LINE_HEIGHT: 0.8,
-  TEXTAREA_MARGIN_BLOCK_START: '0',
+  // FONT_SIZE: '1.5rem',
+  // FONT_FAMILY: 'Nanum Pen Script',
+  // LINE_HEIGHT_FACTOR_IN_CANVAS: 1.2,
+  // TEXTAREA_LINE_HEIGHT: 0.8,
+  // TEXTAREA_MARGIN_BLOCK_START: '0',
   // * --- Case 2: 'K2D'
-  // FONT_SIZE: '1rem',
-  // LINE_HEIGHT_FACTOR_IN_CANVAS: 1.6,
-  // TEXTAREA_LINE_HEIGHT: 1.3,
-  // TEXTAREA_MARGIN_BLOCK_START: '-0.25em',
+  FONT_SIZE: '1rem',
+  FONT_FAMILY: 'K2D',
+  LINE_HEIGHT_FACTOR_IN_CANVAS: 1.85,
+  TEXTAREA_LINE_HEIGHT: 1.5,
+  TEXTAREA_MARGIN_BLOCK_START: '-0.25em',
 
   // * perfect-freehand settings
   PENCIL_SIZE: 3,
